0acae90e2b81bdf25a78504509af9ded5f9dd0d6
knoxrover <vinayrajcheekoti1@gmail.com>
2021-07-25 23:46:42 +0530

    added for js

--- FileSystem_Organiser/commands/ftypes.js
+++ FileSystem_Organiser/commands/ftypes.js
@@ -3,6 +3,7 @@ ftypes.types = {
     media: ["mp4", "mkv","mp3"],
     archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
     documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
-    app: ['exe', 'dmg', 'pkg', "deb"]
+    app: ['exe', 'dmg', 'pkg', "deb"],
+    jsFiles: ['js']
 }
 module.exports = ftypes;
