e4f6bfb3bdc629813aba0ad878f77a797a8f4c4a
knoxrover <vinayrajcheekoti1@gmail.com>
2021-07-26 00:03:52 +0530

    build done for file system organizer

--- FileSystem_Organiser/commands/ftypes.js
+++ FileSystem_Organiser/commands/ftypes.js
@@ -1,9 +1,8 @@
-let ftypes ={}
-ftypes.types = {
+let types = {
     media: ["mp4", "mkv","mp3"],
     archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
     documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
     app: ['exe', 'dmg', 'pkg', "deb"],
     jsFiles: ['js']
 }
-module.exports = ftypes;
+module.exports = types;
