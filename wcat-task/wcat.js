6e4b90cd7e8a95b7982f34137f7861100c753c18
knoxrover <vinayrajcheekoti1@gmail.com>
2021-07-31 13:57:31 +0530

    wcat files base case contents

--- wcat-task/wcat.js
+++ wcat-task/wcat.js
@@ -2,5 +2,21 @@ let fs = require("fs");
 let path = require("path");
 
 
-let inputArr = process.argv(2);
+let inputArr = process.argv.slice(2);
 
+let filesArr = inputArr;
+
+for(let i=0;i<filesArr.length;i++){
+    let ans = fs.existsSync(filesArr[i])
+    if(ans==false){
+        console.log("File doesn't exist");
+        return;
+    }
+}
+
+let content="";
+for(let i=0;i<filesArr.length;i++){
+    content=content +"\n\n"+fs.readFileSync(filesArr[i]);
+}
+
+console.log(content);
