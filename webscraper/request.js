6a40eee2d62a9900f714f06cda34364077426ac0
knoxrover <vinayrajcheekoti1@gmail.com>
2021-08-07 13:06:17 +0530

    cheerio

--- webscraper/request.js
+++ webscraper/request.js
@@ -1,5 +1,9 @@
+const { Cheerio } = require("cheerio");
 let request = require("request");
-request('www.google.com',cb);
+
+let url = 'https://www.espncricinfo.com/series/ipl-2020-21-1210595/royal-challengers-bangalore-vs-sunrisers-hyderabad-eliminator-1237178/ball-by-ball-commentary';
+
+request(url,cb);
 
 function cb(error,response,html){
     //console.log(error) displays if any error
@@ -12,11 +16,31 @@ else if(response.statusCode == 404){
     console.log("page not found")
 }
 else{
-console.log(html); //print html if no errors
+dataExtr(html); //print html if no errors
 }
 }
 
-console.log("After");
 
+
+
+//function cb executes after last console.log(after)
 //request will call google and then wait for cb function to execute and return smtg 
-// cb function takes response ,error, html as arguments from request output and 
+// cb function takes response ,error, html as arguments from request output and executes at end
+
+function dataExtr(html){
+    //search tool
+    let searchTool = Cheerio.load(html);
+    //css selectors to spot elements
+    let elemRepArr = searchTool(".match-comment-wrapper .match-comment-long-text"); // accessing whole element div class 
+
+    // console.log(elemRep);
+
+    let lbc = searchTool(elemRepArr[0]).text(); // from elemrep array load again top element to load commentary for last ball dynamic match updates
+    
+    console.log("lbc : ",lbc);
+
+    //text extract
+    // let moduleName = elemRep.text().trim();
+    // console.log(moduleName);
+}
+console.log("After");
