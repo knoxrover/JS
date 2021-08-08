5f21fda68817f67eb6cae0110815a4823382eb81
knoxrover <vinayrajcheekoti1@gmail.com>
2021-08-08 14:59:40 +0530

    own project

---
+++ webscraper/crypto.js
@@ -0,0 +1,12 @@
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+let request = require("request");
+
+let url1 = "https://coinmarketcap.com/";
+
+request(url1,cb);
+
+function cb(error,response,html){
+    
+}
