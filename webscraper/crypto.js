8838ca8ee314f8ac595dc714f351c1734b69081c
knoxrover <vinayrajcheekoti1@gmail.com>
2021-08-08 15:38:20 +0530

    cb function

--- webscraper/crypto.js
+++ webscraper/crypto.js
@@ -8,5 +8,20 @@ let url1 = "https://coinmarketcap.com/";
 request(url1,cb);
 
 function cb(error,response,html){
-    
+    //console.log(error) displays if any error
+    //console.log('body:',html) displays html
+
+if(error){
+    console.log(error);
 }
+else if(response.statusCode == 404){
+    console.log("page not found")
+}
+else{
+priceExt(html); 
+}
+
+}
+
+
+
