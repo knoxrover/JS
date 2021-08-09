9b34fbfac3a7b13b50dfc65acbcd96fbda3a4972
knoxrover <vinayrajcheekoti1@gmail.com>
2021-08-09 17:07:17 +0530

    scoreCard.js

--- webscraper/activity/scoreCard.js
+++ webscraper/activity/scoreCard.js
@@ -3,3 +3,25 @@ const fs = require("fs");
 
 let request = require("request");
 
+let url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/mumbai-indians-vs-chennai-super-kings-1st-match-1216492/full-scorecard";
+
+
+request(url,cb2);
+
+function cb2(error,response,html){
+
+    if(error){
+        console.log(error);
+    }
+    else if(response.statusCode == 404){
+        console.log("page not found")
+    }
+    else{
+    scoreExt(html);
+    }
+    
+    }
+
+    function scoreExt(html){
+        
+    }
