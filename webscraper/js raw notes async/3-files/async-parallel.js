8082964d971f2c63913fc77f34514a35dcba42ec
knoxrover <vinayrajcheekoti1@gmail.com>
2021-08-23 02:21:12 +0530

    async parallel

--- webscraper/js raw notes async/3-files/async-parallel.js
+++ webscraper/js raw notes async/3-files/async-parallel.js
@@ -4,6 +4,9 @@ fs.readFile("file1.txt",cb);
 fs.readFile("file2.txt",cb1);
 fs.readFile("file3.txt",cb2);
 
+//async -> so no sync word and needs cb
+//parallel -> so each cb not linked with each other 
+
 function cb(err,content){
     console.log("content "+content);
 }
