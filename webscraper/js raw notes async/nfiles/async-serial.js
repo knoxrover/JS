e60942a7f3e8b10fc7560ff9ccf3dee742145052
knoxrover <vinayrajcheekoti1@gmail.com>
2021-08-28 10:59:28 +0530

    deadlock -async arch - forloop

---
+++ webscraper/js raw notes async/nfiles/async-serial.js
@@ -0,0 +1,25 @@
+let fs = require('fs');
+const { features } = require('process');
+
+let files = ["f1.txt","f2.txt","f3.txt","f4.txt"];
+
+//parallel read using async function
+
+// start the work
+
+//also give a cb function from which we can initimitate you after the work is completed
+
+console.log("before");
+
+
+//output
+//first file read then -> cb call
+
+/// deadlock wrong executed -> https://prnt.sc/1qnomqm
+
+for(let i=0;i<files.length;){
+fs.readFile(files[i],function cb(err,data){
+    console.log("data"+data);
+    i++;
+});
+}
