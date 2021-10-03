2f42386e9f086cf37f482d4cb8796a57d3779d7e
knox rover <vinayrajcheekoti1@gmail.com>
2021-10-03 22:43:35 +0530

    pdf lib match pdfs

---
+++ sumeetsirdev/FirstFolderCreation.js
@@ -0,0 +1,31 @@
+//npm install pdf-lib
+//node FirstFolderCreation.js --source="" --dest="";
+
+
+const { create } = require("domain");
+let fs = require("fs");
+let minimist = require("minimist");
+let path = require("path");
+let pdf = require("pdf-lib");
+
+let args = minimist(process.argv);
+
+let teamsJson = fs.readFileSync(args.source, "utf-8");
+let teams = JSON.parse(teamsJson);
+
+for (let i = 0; i < teams.length; i++) {
+  let folderName = path.join(args.dest, teams[i].name);
+  console.log(folderName);
+ 
+//   fs.mkdirSync(folderName);
+  for(let j=0;j<teams[i].length;j++){
+      console.log(teams[i].matches[j]);
+    //   let fileName = path.join(folderName,teams[i].matches[j].vs+".pdf");
+    //   createScoreCard(teams[i].name,teams[i].matches[j],fileName);
+  }
+}
+
+function createScoreCard(teamName,match,matchFileName){
+
+
+}
