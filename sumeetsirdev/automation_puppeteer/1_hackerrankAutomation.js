f4cfc45fd39430164fdc6ad0206f12837d589974
knox rover <vinayrajcheekoti1@gmail.com>
2021-10-22 08:44:52 +0530

    random

--- sumeetsirdev/automation_puppeteer/1_hackerrankAutomation.js
+++ sumeetsirdev/automation_puppeteer/1_hackerrankAutomation.js
@@ -1,51 +1,123 @@
-let minimist = require('minimist');
-let fs = require('fs');
-let puppeteer = require('puppeteer');
+// node 1_HackerrankAutomation.js --url=https://www.hackerrank.com --config=config.json 
 
+// npm init -y
+// npm install minimist
+// npm install puppeteer 
 
-let args= minimist(process.argv);
-let configJSON = fs.readFileSync(args.config,"utf-8");
-let config=JSON.parse(configJSON);
+let minimist = require("minimist");
+let puppeteer = require("puppeteer");
+let fs = require("fs");
 
-async function run(){
+let args = minimist(process.argv);
+
+let configJSON = fs.readFileSync(args.config, "utf-8");
+let configJSO = JSON.parse(configJSON);
+
+async function run() {
+    // start the browser
     let browser = await puppeteer.launch({
-        
-        args:[
+        headless: false,
+        args: [
             '--start-maximized'
         ],
-        headless:false,
-        defaultViewport:{
-            width:1920,
-            height:1080,
-            isMobile:false
-        }
+        defaultViewport: null
     });
+
+    // get the tabs (there is only one tab)
     let pages = await browser.pages();
-    let page=pages[0];
+    let page = pages[0];
+
+    // open the url
     await page.goto(args.url);
 
-    await page.click("li#menu-item-2887");
-    await page.waitForNavigation();
-    
+    // wait and then click on login on page1
+    await page.waitForSelector("a[data-event-action='Login']");
+    await page.click("a[data-event-action='Login']");
 
-    await page.click("a.f1-button[href='www.hackerrank.com/login']");
-    await page.waitForNavigation();
+    // wait and then click on login on page2
+    await page.waitForSelector("a[href='https://www.hackerrank.com/login']");
+    await page.click("a[href='https://www.hackerrank.com/login']");
 
-    await page.waitFor(5000);
+    // type userid
+    await page.waitForSelector("input[name='username']");
+    await page.type("input[name='username']", configJSO.userid, { delay: 20 });
+
+    // type password
+    await page.waitForSelector("input[name='password']");
+    await page.type("input[name='password']", configJSO.password, { delay: 20 });
+
+    await page.waitFor(3000);
+
+    // press click on page3
+    await page.waitForSelector("button[data-analytics='LoginPassword']");
+    await page.click("button[data-analytics='LoginPassword']");
+
+    // click on compete
+    await page.waitForSelector("a[data-analytics='NavBarContests']");
+    await page.click("a[data-analytics='NavBarContests']");
+
+    // click on manage contests
+    await page.waitForSelector("a[href='/administration/contests/']");
+    await page.click("a[href='/administration/contests/']");
 
 
-    await page.click("div.cookie-btn-wrapper > button");
+    // find number of pages
+    await page.waitForSelector("a[data-attr1='Last']");
+    let numPages = await page.$eval("a[data-attr1='Last']", function (atag) {
+        let totPages = parseInt(atag.getAttribute("data-page"));
+        return totPages;
+    });
 
-    //tab thrice
+    for (let i = 1; i <= numPages; i++) {
+        await handleAllContestsOfAPage(page, browser);
 
-    for(let i=0;i<3;i++){
-        await page.keyboard.press("Tab",{delay:300});
+        if (i != numPages) {
+            await page.waitForSelector("a[data-attr1='Right']");
+            await page.click("a[data-attr1='Right']");
+        }
     }
-
-
-    await page.keyboard.type(config.userid, {delay:100});
-    await page.keyboard
-  
-
-
 }
+
+async function handleAllContestsOfAPage(page, browser) {
+    // find all urls of same page
+    await page.waitForSelector("a.backbone.block-center");
+    let curls = await page.$$eval("a.backbone.block-center", function (atags) {
+        let urls = [];
+
+        for (let i = 0; i < atags.length; i++) {
+            let url = atags[i].getAttribute("href");
+            urls.push(url);
+        }
+
+        return urls;
+    });
+
+    for (let i = 0; i < curls.length; i++) {
+        let ctab = await browser.newPage();
+        await saveModeratorInContest(ctab, args.url + curls[i], configJSO.moderator);
+        await ctab.close();
+        await page.waitFor(3000);
+    }
+}
+
+async function saveModeratorInContest(ctab, fullCurl, moderator) {
+    await ctab.bringToFront();
+    await ctab.goto(fullCurl);
+    await ctab.waitFor(3000);
+
+    // click on moderators tab
+    await ctab.waitForSelector("li[data-tab='moderators']");
+    await ctab.click("li[data-tab='moderators']");
+
+    // type in moderator
+    await ctab.waitForSelector("input#moderator");
+    await ctab.type("input#moderator", moderator, { delay: 50 });
+
+    // press enter
+    await ctab.keyboard.press("Enter");
+}
+
+
+
+run();
+
