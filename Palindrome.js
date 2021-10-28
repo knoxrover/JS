9ca654dc7b1677fa34ca986346b79d447523cb7f
Bhanu <87806055+Bhanu-Nayak@users.noreply.github.com>
2021-10-28 13:07:19 +0530

    Palindrome
    Js program to check palindrome

---
+++ Palindrome.js
@@ -0,0 +1,23 @@
+function checkPalindrome(str) {
+
+
+    const len = string.length;
+
+
+    for (let i = 0; i < len / 2; i++) {
+
+     
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
+}
+
+
+const string = prompt('Enter a string: ');
+
+
+const value = checkPalindrome(string);
+
+console.log(value);
